94ec23cac7f0aef0cbd77b55f8cdd0230cb8de4b
Jason Vander Woude <jasonvwoude@gmail.com>
2018-02-14 12:09:41 -0600

    empty code

--- time-lapse.py
+++ time-lapse.py
@@ -34,62 +34,62 @@ import requests
 #                       data=payload, headers=headers)
 
 
-def image_filename():
-    'Prepare filename with timestamp.'
-    epoch = int(time())
-    filename = '/tmp/images/{timestamp}.jpg'.format(timestamp=epoch)
-    return filename
+# def image_filename():
+#     'Prepare filename with timestamp.'
+#     epoch = int(time())
+#     filename = '/tmp/images/{timestamp}.jpg'.format(timestamp=epoch)
+#     return filename
 
-def usb_camera_photo():
-    'Take a photo using a USB camera.'
-    import cv2
-    # Settings
-    camera_port = 0      # default USB camera port
-    discard_frames = 20  # number of frames to discard for auto-adjust
+# def usb_camera_photo():
+#     'Take a photo using a USB camera.'
+#     import cv2
+#     # Settings
+#     camera_port = 0      # default USB camera port
+#     discard_frames = 20  # number of frames to discard for auto-adjust
 
-    # Check for camera
-    if not os.path.exists('/dev/video' + str(camera_port)):
-        print("No camera detected at video{}.".format(camera_port))
-        camera_port += 1
-        print("Trying video{}...".format(camera_port))
-        if not os.path.exists('/dev/video' + str(camera_port)):
-            print("No camera detected at video{}.".format(camera_port))
-            log("USB Camera not detected.", "error")
+#     # Check for camera
+#     if not os.path.exists('/dev/video' + str(camera_port)):
+#         print("No camera detected at video{}.".format(camera_port))
+#         camera_port += 1
+#         print("Trying video{}...".format(camera_port))
+#         if not os.path.exists('/dev/video' + str(camera_port)):
+#             print("No camera detected at video{}.".format(camera_port))
+#             log("USB Camera not detected.", "error")
 
-    # Open the camera
-    camera = cv2.VideoCapture(camera_port)
-    sleep(0.1)
+#     # Open the camera
+#     camera = cv2.VideoCapture(camera_port)
+#     sleep(0.1)
 
-    # Let camera adjust
-    for _ in range(discard_frames):
-        camera.grab()
+#     # Let camera adjust
+#     for _ in range(discard_frames):
+#         camera.grab()
 
-    # Take a photo
-    ret, image = camera.read()
+#     # Take a photo
+#     ret, image = camera.read()
 
-    # Close the camera
-    camera.release()
+#     # Close the camera
+#     camera.release()
 
-    # Output
-    if ret:  # an image has been returned by the camera
-        # Save the image to file
-        cv2.imwrite(image_filename(), image)
-        print("Image saved: {}".format(image_filename()))
-    else:  # no image has been returned by the camera
-        log("Problem getting image.", "error")
+#     # Output
+#     if ret:  # an image has been returned by the camera
+#         # Save the image to file
+#         cv2.imwrite(image_filename(), image)
+#         print("Image saved: {}".format(image_filename()))
+#     else:  # no image has been returned by the camera
+#         log("Problem getting image.", "error")
 
-def rpi_camera_photo():
-    'Take a photo using the Raspberry Pi Camera.'
-    from subprocess import call
-    try:
-        retcode = call(
-            ["raspistill", "-w", "640", "-h", "480", "-o", image_filename()])
-        if retcode == 0:
-            print("Image saved: {}".format(image_filename()))
-        else:
-            log("Problem getting image.", "error")
-    except OSError:
-        log("Raspberry Pi Camera not detected.", "error")
+# def rpi_camera_photo():
+#     'Take a photo using the Raspberry Pi Camera.'
+#     from subprocess import call
+#     try:
+#         retcode = call(
+#             ["raspistill", "-w", "640", "-h", "480", "-o", image_filename()])
+#         if retcode == 0:
+#             print("Image saved: {}".format(image_filename()))
+#         else:
+#             log("Problem getting image.", "error")
+#     except OSError:
+#         log("Raspberry Pi Camera not detected.", "error")
 
 
 
@@ -101,10 +101,10 @@ if __name__ == '__main__':
     # except (KeyError, ValueError):
     #     CAMERA = 'USB'  # default camera
 
-    if 'RPI' in CAMERA:
-        rpi_camera_photo()
-    else:
-        usb_camera_photo()
+    # if 'RPI' in CAMERA:
+    #     rpi_camera_photo()
+    # else:
+    #     usb_camera_photo()
 
     # API_TOKEN = os.environ['API_TOKEN']
 
